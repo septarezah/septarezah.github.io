82e0f4fb64b3a63e1458c682c353087b92581f3d
Septa Reza Hardiansyah <46550673+serehard@users.noreply.github.com>
2022-09-23 13:47:32 +0700

    Upload Portfolio

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Navbar fixed
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+
+// Klik diluar hamburger
+
+window.addEventListener('click', function(e){
+if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+}
+});
+
+
+//Darkmode toggle
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';        
+    } else {
+        html.classList.remove('light');
+        localStorage.theme = 'light'; 
+    }
+});
+
+// Pindahkan posisi toggle checkbox
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = 'false';
+  } else {
+    darkToggle.checked = 'true';
+  }
